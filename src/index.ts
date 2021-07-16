61f8c913aece088317586100b0e262987f5d9b62
Dusan Turajlic <45139784+dusan-turajlic@users.noreply.github.com>
2021-07-16 09:58:02 +0300

    (HyperMashmau) Implaments simplest happy day senario (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,101 @@
-const world = 'world';
+import { JsonPointer } from 'json-ptr';
+import { Resource } from 'halfred';
 
-export function hello(w: string = world): string {
-    return `Hello ${w}! `;
+export interface HyperMashmauParams {
+    httpClient: HttpClient;
+    apiRootUrl: string;
+}
+
+const START_CONTENT_CHARACTER = '{';
+const END_CONTENT_CHARACTER = '}';
+
+export class HyperMashmau {
+    private httpClient: HttpClient;
+    private apiRoot?: Resource;
+    private readonly rootRequest: Promise<Record<string, unknown>>;
+
+    constructor({ httpClient, apiRootUrl }: HyperMashmauParams) {
+        if (!httpClient) {
+            throw new Error('httpClient is required');
+        }
+        this.httpClient = httpClient;
+
+        this.httpClient.get(apiRootUrl);
+        this.rootRequest = this.httpClient.getResponse();
+    }
+
+    async get<T = Record<string, unknown>>(pointer: string): Promise<any> {
+        await this.isRootPending();
+        const { data, jsonPointer } = this.getJsonPointerAndData(pointer);
+        const embedded = this.apiRoot?.allEmbeddedResourceArrays();
+        let resources = jsonPointer.get(embedded) as Resource | Resource[];
+        if (resources) {
+            if (!Array.isArray(resources)) {
+                resources = [resources] as Resource[];
+            }
+
+            return Promise.all(
+                resources.map(async (resource) => {
+                    if (this.hasAllKeys(resource, data)) {
+                        return this.makeResponse<T>(resource, data);
+                    } else {
+                        const self = resource.link('self');
+                        this.httpClient.get(self);
+                        return this.makeResponse<T>(await this.httpClient.getResponse(), data);
+                    }
+                }),
+            ).then((response) => {
+                if (response.length === 1) {
+                    return response[0];
+                }
+
+                return response;
+            });
+        }
+
+        return Promise.reject(`Cannot find resource matching pattern ${pointer}`);
+    }
+
+    private getJsonPointerAndData(pointer: string): { data: string[]; jsonPointer: JsonPointer } {
+        pointer = pointer.replace(/(\s+?\r\n|\s+?\n|\r|\s)/gm, '');
+        const start = pointer.indexOf(START_CONTENT_CHARACTER);
+        const end = pointer.lastIndexOf(END_CONTENT_CHARACTER);
+        const data = pointer.slice(start + 1, end).split(',');
+        if (data[data.length - 1] === '') {
+            data.pop();
+        }
+
+        let jsonPointer = pointer.slice(0, start - 1);
+        if (jsonPointer.slice(jsonPointer.length - 1) === '/') {
+            // in case depth is not specified it will return everything
+            jsonPointer += '-';
+        }
+
+        return { data, jsonPointer: new JsonPointer(jsonPointer) };
+    }
+
+    private async makeResponse<T>(responseData: Resource, data: string[]): Promise<T> {
+        return new Promise<T>((resolve) => {
+            resolve(
+                data.reduce((memo, key) => {
+                    Object.defineProperty(memo, key, {
+                        get() {
+                            return (responseData as unknown as Record<string, unknown>)[key];
+                        },
+                    });
+                    return memo;
+                }, {}) as unknown as T,
+            );
+        });
+    }
+
+    private async isRootPending() {
+        this.apiRoot = (await this.rootRequest) as unknown as Resource;
+    }
+
+    private hasAllKeys(object: Resource, keys: string[]): boolean {
+        const objectKeys = Object.keys(object);
+        // Returns false if the keys are not present in the object
+        return keys.filter((key) => objectKeys.includes(key)).length === keys.length;
+    }
 }
---
+++ test/mocks/MockFsAPI.ts
@@ -0,0 +1,81 @@
+import { parse, Resource } from 'halfred';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const FAKE_DOMAIN = 'https://exaple.org';
+
+export default class MockFsAPI implements HttpClient {
+    private activePromise?: Promise<Record<string, unknown>>;
+
+    delete(arg: HttpClientUrl): void {
+        throw new Error('@TOOD: implament');
+    }
+
+    get(arg: HttpClientUrl): void {
+        this.activePromise = this.readFile(MockFsAPI.toUrl(arg));
+    }
+
+    head(arg: HttpClientUrl): void {
+        throw new Error('@TOOD: implament');
+    }
+
+    options(arg: HttpClientUrl): void {
+        throw new Error('@TOOD: implament');
+    }
+
+    patch(arg: HttpClientUrl): void {
+        throw new Error('@TOOD: implament');
+    }
+
+    post(arg: HttpClientUrl): void {
+        throw new Error('@TOOD: implament');
+    }
+
+    put(arg: HttpClientUrl): void {
+        throw new Error('@TOOD: implament');
+    }
+
+    async getResponse(): Promise<Resource> {
+        if (!this.activePromise) {
+            throw new Error('No active promises, are you sure you made a request?');
+        }
+        return parse(await this.getActivePromise());
+    }
+
+    private static toUrl(arg: HttpClientUrl): URL {
+        if (typeof arg !== 'string') {
+            arg = (arg as Self).href;
+        }
+
+        if (!arg.includes('http') || !arg.includes('https')) {
+            arg = FAKE_DOMAIN + arg;
+        }
+
+        return new URL(arg);
+    }
+
+    private async readFile(url: URL): Promise<Record<string, unknown>> {
+        return new Promise((resolve, reject) => {
+            /**
+             * The `path` should resemble the mock api directory structure.
+             * @example
+             *  // If the url looks like this
+             *  http://example.org/api/user/0
+             *  // The readFile path paramater should looks like this
+             *  fs.readFile(`./api/user/0.json`)
+             */
+            fs.readFile(`${__dirname}${url.pathname}.json`, 'utf8', function (err: unknown, data: string) {
+                if (err) {
+                    reject(err);
+                }
+                resolve(JSON.parse(data));
+            });
+        });
+    }
+
+    private async getActivePromise(): Promise<Record<string, unknown>> {
+        const response = (await this.activePromise) as Record<string, unknown>;
+        this.activePromise = undefined;
+        return response;
+    }
+}
